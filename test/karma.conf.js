693a50cf8677737b6a7957a58b5fc9cbfb6e8002
Juhász Viktor <viktor.juhasz@balabit.com>
2015-11-20 17:26:02 +0100

    karma intergration

---
+++ test/karma.conf.js
@@ -0,0 +1,27 @@
+module.exports = function(config){
+  config.set({
+
+    basePath : '../',
+
+    files : [
+      'test/unit/*.js'
+    ],
+
+    autoWatch : true,
+
+    frameworks: ['jasmine'],
+
+    browsers : ['PhantomJS'],
+
+    plugins : [
+            'karma-phantomjs-launcher',
+            'karma-jasmine'
+            ],
+
+    junitReporter : {
+      outputFile: 'test_out/unit.xml',
+      suite: 'unit'
+    }
+
+  });
+};
